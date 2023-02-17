54bc2e19693dc1120e012a9b9bbc8c8b3020608f
CanvasL <746591811@qq.com>
2023-02-18 00:04:00 +0800

    feat: new wallet

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,7 @@
 import Balance from "./Balance";
 import LanguageSelector from "./LanguageSelector";
 import CryptoList from "./CryptoList";
+import WalletCreate from "./WalletCreate";
 import WalletRecovery from "./WalletRecovery";
 import InputLabel from "./InputLabel";
 
@@ -8,6 +9,7 @@ export {
     Balance,
     CryptoList,
     LanguageSelector,
+    WalletCreate,
     WalletRecovery,
     InputLabel
 }
