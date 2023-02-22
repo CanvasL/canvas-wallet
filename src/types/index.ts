90a040758165f673e5d302a5595b864606264869
CanvasL <746591811@qq.com>
2023-02-22 23:04:37 +0800

    feat: add multisig wallet init

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,5 +4,5 @@ export {
     RecoveryType
 };
 export type { 
-    IContent 
+    IContent,
 };
---
+++ src/types/store/index.ts
@@ -0,0 +1,9 @@
+// enum INetwork {
+//     mainnet,
+//     goerli,
+//     sepolia
+// }
+
+export {
+    // INetwork
+}
