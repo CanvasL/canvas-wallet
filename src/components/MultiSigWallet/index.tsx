8f8b036dfda3e68a15c841cfae2fcf96d9467b4e
CanvasL <746591811@qq.com>
2023-02-22 23:04:37 +0800

    feat: add multisig wallet init

--- src/components/MultiSigWallet/index.tsx
+++ src/components/MultiSigWallet/index.tsx
@@ -1,8 +1,97 @@
+import './index.scss';
+import { useState } from 'react';
+import { Button, Form, Grid, Input, Label, Menu, MenuItemProps, Segment } from 'semantic-ui-react';
+import { useStore } from '../../store';
+import { shortenAddress } from '../../utils';
+
 const MultiSigWallet = () => {
+    const { walletStore } = useStore();
+    const [state, setState] = useState({ activeItem: 'create'});
+
+    const handleItemClick = (_: any, { name }: MenuItemProps) => {
+        setState({ activeItem: name as string })
+    }
+
+    const { activeItem } = state;
     return (
-        <>
-        MultiSigWallet
-        </>
+        <Grid>
+            <Grid.Column width={6}>
+                <Menu size='large' vertical>
+                    {
+                        (walletStore.multiSigWalletAddress || []).map((address, index) => {
+                            return (
+                                <Menu.Item
+                                    key={index}
+                                    name={address}
+                                    active={activeItem === address}
+                                    onClick={handleItemClick}
+                                >
+                                    <Label color='teal'>{index + 1}</Label>
+                                    {shortenAddress(address)}
+                                </Menu.Item>
+                            )
+                        })
+                    }
+                    {/* <Menu.Item
+                        name='inbox'
+                        active={activeItem === 'inbox'}
+                        onClick={handleItemClick}
+                    >
+                        <Label color='teal'>1</Label>
+                        Inbox
+                    </Menu.Item>
+
+                    <Menu.Item
+                        name='spam'
+                        active={activeItem === 'spam'}
+                        onClick={handleItemClick}
+                    >
+                        <Label>51</Label>
+                        Spam
+                    </Menu.Item>
+
+                    <Menu.Item
+                        name='updates'
+                        active={activeItem === 'updates'}
+                        onClick={handleItemClick}
+                    >
+                        <Label>1</Label>
+                        Updates
+                    </Menu.Item> */}
+                    <Menu.Item
+                        name='create'
+                        active={activeItem === 'create'}
+                    >
+                        <Button basic color='teal'>Create</Button>
+                    </Menu.Item>
+                </Menu>
+            </Grid.Column>
+            <Grid.Column width={9}>
+                <Segment stacked>
+                    <Form>
+                        <Form.Field>
+                            <label>Num Confirmations Required</label>
+                            <Input placeholder='0' />
+                        </Form.Field>
+                        <Form.Field>
+                            <label>Owners</label>
+                            <Input icon='settings' iconPosition='left' disabled placeholder='0x' />
+                        </Form.Field>
+                        <Form.Field>
+                            <Input icon='settings' iconPosition='left' placeholder='0x' />
+                        </Form.Field>
+                        <Form.Field>
+                            <Button
+                                content='Add New Owner'
+                                icon='add'
+                                labelPosition='left'
+                            />
+                        </Form.Field>
+                        {/* <Button color='teal' type='submit'>Create</Button> */}
+                    </Form>
+                </Segment>
+            </Grid.Column>
+        </Grid>
     )
 }
 
--- src/store/WalletStore.tsx
+++ src/store/WalletStore.tsx
@@ -1,10 +1,14 @@
 import { makeAutoObservable } from "mobx";
-import { ethers } from "ethers";
+import { ethers, Contract } from "ethers";
 import { RecoveryType } from "../types";
 import { http } from '../utils';
+import walletDeclareFile from '../deployments/MultiSigWallet.json';
 
 class WalletStore {
+    network?: 'homestead' | 'goerli';
     wallet?: ethers.Wallet | ethers.HDNodeWallet;
+    canvasWallet?: Contract;
+    multiSigWalletAddress?: string[];
     provider?: ethers.Provider;
     balance?: bigint;
     prices?: any;
@@ -12,6 +16,12 @@ class WalletStore {
 
     constructor() {
         makeAutoObservable(this);
+        this._initNetwork();
+        this._initWallet();
+    }
+    
+    getMultiSigWallet = (address: string) => {
+        return new ethers.Contract(address, walletDeclareFile.abi, this.provider);
     }
 
     importWallet = async (type: RecoveryType, key: string) => {
@@ -21,10 +31,48 @@ class WalletStore {
         } else {
             this.wallet = ethers.Wallet.fromPhrase(key);
         }
-        this.provider = new ethers.InfuraProvider();
-        this.balance = await this.provider.getBalance(this.wallet.address);
-        // this.transactionHistory = await this.provider.getHistory(this.wallet.address);
+        this._initProvider();
+        await this._initBalance();
+        await this._initPrices();
+    }
+
+    initCanvasWallet = async () => {
+        const walletFactoryDeclareFile = require(`../deployments/${this.network}/MultiSigWalletFactory.json`);
+        this.canvasWallet = new ethers.Contract(walletFactoryDeclareFile.address, walletFactoryDeclareFile.abi, this.provider);
+        await this._initMultiSigWalletsAddress();
+    }
+
+    initNetwork = async (network: 'homestead' | 'goerli') => {
+        this.network = network;
+        localStorage.setItem('canvas-wallet--network', network);
+    }
+
+    private _initWallet = async () => {
+        this.importWallet(RecoveryType.MnemonicPhrase, 'fringe phrase river ostrich sail climb kingdom weasel palace gas party flight');
+    }
+
+    private _initNetwork = () => {
+        this.network = localStorage.getItem('canvas-wallet--network') as any || 'homestead';
+    }
+
+    private _initProvider = () => {
+        this.provider = new ethers.AlchemyProvider(this.network, 'e2Q3GChQV5hFnhhbSX9tnYKr3e3WWgFt');
+    }
+
+    private _initBalance = async () => {
+        this.balance = await this.provider!.getBalance(this.wallet!.address!);
+    }
+
+    private _initPrices = async () => {
         this.prices = await http.get('simple/price?ids=ethereum&vs_currencies=usd');
+    }
+
+    private _initTransactionHistory = async () => {
+        // this.transactionHistory = await this.provider!.getHistory(this.wallet.address);
+    }
+
+    private _initMultiSigWalletsAddress = async () => {
+        this.multiSigWalletAddress = await this.canvasWallet!.getWalletsByCreater(this.wallet!.address);
     }
 }
 
