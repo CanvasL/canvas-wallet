a755e3bbfb8f66bb047d1442a0e70aaa5a6c9dff
CanvasL <746591811@qq.com>
2023-02-27 13:18:45 +0800

    feat: add GetRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import './App.scss';
-import { Routes, Route } from 'react-router-dom';
 import {Header} from './layouts';
-import { Dashboard, Transactions, Settings } from './pages';
+import GetRoutes from './router/GetRoutes';
 
 const App = () => {
   return (
       <div className='App'>
         <Header />
-        <Routes>
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/transactions' element={<Transactions />} />
-          <Route path='/settings' element={<Settings />} />
-        </Routes>
+        <GetRoutes />
       </div>
   );
 }
